2de70faba899f892edd21b41240ac6219492fe17
Vonorss <85298758+Vonorss@users.noreply.github.com>
2022-05-19 18:27:17 -0400

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    tooltip1: "Twitch",
+    link1: "https://www.twitch.tv/banknotes5m",
+
+    tooltip2: "Instagram",
+    link2: "https://www.instagram.com/luhwntrrs/",
+
+    tooltip3: "Bank.#0666",
+    link3: "https://discord.com/users/876585317489066044",
+
+    userName: "Bank",
+
+    cards: [
+      {
+        title: "RuinRZ",
+        link: "https://discord.gg/rz",
+        role: "Staff",
+        purpose: "Fivem Gaming",
+        paragraph:
+          "Ruin is a place to meet friends and also is a Fivem PVP server",
+      },
+      {
+        title: "Kraken",
+        link: "https://kraken.pics",
+        role: "Contributor",
+        purpose: "Public File Uploader",
+        paragraph:
+          "Kraken is a public file-uploader, used to store files and customise embeds, with the choice of custom domains, allowing you to personalise your images your way.",
+      },
+      {
+        title: "E-Z.host",
+        link: "https://e-z.host",
+        role: "Contributor",
+        purpose: "Private File Uploader",
+        paragraph:
+          "E-Z.host is a closed-private file-uploader, used to store uploads and customise embeds, it has a plethora of features to choose from allowing you to personalise your files, you can purchase it on their website.",
+      },
+    ],
+  },
+};
